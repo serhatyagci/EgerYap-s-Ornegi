669157447103a5149f5d0625264f84ba1a423bfc
serhatyagci <serhatyagci1@gmail.com>
2022-11-12 19:36:32 +0300

    Add project files.

---
+++ EgerYapısıOrnegi/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EgerYapısıOrnegi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lütfen 1. notunuzu giriniz");
+            int birinci = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Lütfen 2. notunuzu giriniz");
+            int ikinci = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Lütfen 3. notunuzu giriniz");
+            int ucuncu = Convert.ToInt32(Console.ReadLine());
+
+            int sonuc = (birinci + ikinci + ucuncu) / 3;
+
+            if (sonuc >= 80 && sonuc < 100)
+            {
+                Console.WriteLine("Not değeriniz : A+");
+            }
+
+            else if (sonuc >= 60)
+            {
+                Console.WriteLine("Not değeriniz : A");
+            }
+
+            else if (sonuc >= 40)
+            {
+                Console.WriteLine("Not değeriniz : B+");
+            }
+
+            else
+            {
+                Console.WriteLine("Not değeriniz : Nanay");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ HesapMakinesi/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HesapMakinesi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double sonuc = 1;
+
+
+            Console.WriteLine("Birinci sayıyı giriniz:");
+            double birinci = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("İkinci sayıyı giriniz:");
+            double ikinci = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Yapılacak işlemi seçiniz(*/-+):");
+            string islem = Console.ReadLine();
+
+
+            if (islem == "*")
+            {
+                sonuc = birinci * ikinci;
+            }
+            else if (islem == "/")
+            {
+                sonuc = birinci / ikinci;
+            }
+            else if (islem == "+")
+            {
+                sonuc = birinci + ikinci;
+            }
+            else if (islem == "-")
+            {
+                sonuc = birinci - ikinci;
+            }
+            else
+            {
+                Console.WriteLine("yanlış işlem seçtiniz.");
+                Console.ReadLine();
+            }
+
+
+            Console.WriteLine("işlemin sonucu: " + sonuc);
+            Console.ReadLine();
+
+        }
+    }
+}
